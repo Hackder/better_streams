8a03f4b4a4eeee515e8e0de7f3cf98c64991f196
Juraj Petras <jurkopetras@gmail.com>
2025-08-20 14:49:58 +0200

    feat: rename sender to emitter

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 use futures::Stream;
 use std::cell::Cell;
-use std::fmt::Debug;
 use std::future::Future;
 use std::sync::Arc;
 use std::task::Poll;
@@ -51,11 +50,11 @@ where
     }
 }
 
-pub struct FnStreamSend {
+pub struct FnStreamEmit {
     sent: bool,
 }
 
-impl Future for FnStreamSend {
+impl Future for FnStreamEmit {
     type Output = ();
 
     fn poll(
@@ -71,11 +70,11 @@ impl Future for FnStreamSend {
     }
 }
 
-pub struct FnStreamSender<T: Send> {
+pub struct FnStreamEmitter<T: Send> {
     value: Value<T>,
 }
 
-impl<T> FnStreamSender<T>
+impl<T> FnStreamEmitter<T>
 where
     T: Send,
 {
@@ -84,24 +83,24 @@ where
     }
 
     #[must_use]
-    pub fn send(&mut self, value: T) -> FnStreamSend {
+    pub fn emit(&mut self, value: T) -> FnStreamEmit {
         let value_cell = self.value.get_or(|| Cell::new(None));
         if value_cell.take().is_some() {
             panic!(
-                "FnStreamSender can only send one value at a time. Previous send was not awaited"
+                "FnStreamEmitter can only send one value at a time. Previous send was not awaited"
             );
         }
 
         value_cell.set(Some(value));
 
-        FnStreamSend { sent: false }
+        FnStreamEmit { sent: false }
     }
 }
 
 pub fn fn_stream<T, F>(f: F) -> FnStream<T, impl Future<Output = ()>>
 where
-    F: for<'a> AsyncFnOnce(&'a mut FnStreamSender<T>) -> (),
-    T: Debug + Send,
+    F: for<'a> AsyncFnOnce(&'a mut FnStreamEmitter<T>) -> (),
+    T: Send,
 {
     let value = Arc::new(thread_local::ThreadLocal::new());
     let value_clone = Arc::clone(&value);
@@ -109,7 +108,7 @@ where
     FnStream {
         value,
         future: async move {
-            let mut sender = FnStreamSender::new(value_clone);
+            let mut sender = FnStreamEmitter::new(value_clone);
             f(&mut sender).await;
         },
     }
@@ -126,7 +125,7 @@ mod tests {
     #[test]
     fn fn_stream_send_is_send() {
         let stream = fn_stream(async |sender| {
-            sender.send(1).await;
+            sender.emit(1).await;
         });
 
         assert_send(stream);
@@ -135,7 +134,7 @@ mod tests {
     #[tokio::test]
     async fn single_send() {
         let stream = fn_stream(async |sender| {
-            sender.send(1).await;
+            sender.emit(1).await;
         });
 
         pin_mut!(stream);
@@ -147,8 +146,8 @@ mod tests {
     #[tokio::test]
     async fn multiple_sends() {
         let stream = fn_stream(async |sender| {
-            sender.send(1).await;
-            sender.send(2).await;
+            sender.emit(1).await;
+            sender.emit(2).await;
         });
 
         pin_mut!(stream);
@@ -161,9 +160,9 @@ mod tests {
     #[tokio::test]
     async fn future_between_sends() {
         let stream = fn_stream(async |sender| {
-            sender.send(1).await;
+            sender.emit(1).await;
             tokio::time::sleep(std::time::Duration::from_millis(1)).await;
-            sender.send(2).await;
+            sender.emit(2).await;
         });
 
         pin_mut!(stream);
@@ -175,16 +174,16 @@ mod tests {
 
     #[tokio::test]
     async fn nested_fn_send() {
-        async fn nested(sender: &mut FnStreamSender<i32>) {
-            sender.send(2).await;
+        async fn nested(sender: &mut FnStreamEmitter<i32>) {
+            sender.emit(2).await;
             tokio::time::sleep(std::time::Duration::from_millis(1)).await;
-            sender.send(3).await;
+            sender.emit(3).await;
         }
 
         let stream = fn_stream(async |mut sender| {
-            sender.send(1).await;
+            sender.emit(1).await;
             nested(&mut sender).await;
-            sender.send(4).await;
+            sender.emit(4).await;
         });
 
         pin_mut!(stream);
@@ -204,8 +203,8 @@ mod tests {
             let a = &a;
             let b = &b;
             let stream = fn_stream(async |sender| {
-                sender.send(a).await;
-                sender.send(b).await;
+                sender.emit(a).await;
+                sender.emit(b).await;
             });
             pin_mut!(stream);
             assert_eq!(Some(a), stream.next().await);
